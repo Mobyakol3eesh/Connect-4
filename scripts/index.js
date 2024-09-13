39bf1ac9539b6c20d88aa54dc46294a84f4c49ff
mobyakol3eesh <mohamed_hesham203@icloud.com>
2024-09-13 05:00:07 +0300

    needs to know when there is a winner

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-class Player{
+class Player {
     number;
     score;
 }
 class Turn {
     player;
     constructor() {
-       this.player = Math.floor(Math.random() * 2) + 1;
+        this.player = Math.floor(Math.random() * 2) + 1;
     }
     changePlayer() {
         this.player = this.player === 1 ? 2 : 1;
@@ -24,32 +24,35 @@ class Circle {
 }
 class RedCircle extends Circle {
     constructor(player) {
-        super(player,'red');
+        super(player, "red");
     }
 }
 class YellowCircle extends Circle {
     constructor(player) {
-        super(player, 'yellow');
+        super(player, "yellow");
     }
 }
 class Board {
-    constructor(rows,cols) {
-        this.boardArr = Array.from({ length: rows }, () => Array(cols).fill(null));
+    boardArr;
+    constructor(rows, cols) {
+        this.boardArr = Array.from({ length: rows }, () =>
+            Array(cols).fill(null)
+        );
     }
 }
 
 const rows = 6;
 const cols = 7;
-const gameBoard = document.querySelector('.game-board');
+const gameBoard = document.querySelector(".game-board");
 
-const player1 = new Player(1,0);
-const player2 = new Player(2,0);
+const player1 = new Player(1, 0);
+const player2 = new Player(2, 0);
 const turn = new Turn();
-const board = new Board(rows,cols);
+const board = new Board(rows, cols);
 
 (function GameLoad() {
     for (let i = 0; i < rows; i++) {
-   gameBoard.innerHTML +=`
+        gameBoard.innerHTML += `
                 <div class="row cells" data-row="${i}">
                 <div class="cell" data-column="0"></div>
                 <div class="cell" data-column="1"></div>
@@ -62,39 +65,93 @@ const board = new Board(rows,cols);
     }
 })();
 
-
-
-
-document.querySelectorAll('.cell').forEach(cell => {
-    cell.addEventListener('click', e => { 
+document.querySelectorAll(".cell").forEach((cell) => {
+    cell.addEventListener("click", (e) => {
         const column = e.target.dataset.column;
-        const playerTurn =  turn.getTurn()
-        insertCircle(getLowestEmptyRow(column),playerTurn);
+        const playerTurn = turn.getTurn();
+        insertCircle(getLowestEmptyRow(column), playerTurn);
     });
 });
+
 function getLowestEmptyRow(column) {
     const cells = document.querySelectorAll(`.cell[data-column="${column}"]`);
     for (let i = cells.length - 1; i >= 0; i--) {
-        if(!cells[i].classList.contains('empty')) {
-            cells[i].classList.add('empty');
+        if (!cells[i].classList.contains("empty")) {
+            cells[i].classList.add("empty");
             console.log(cells[i].parentElement.dataset.row);
             return cells[i];
         }
     }
 }
-function insertCircle(cell,playerTurn) {
-    if(cell) {
-        if(playerTurn === 1) {
+function insertCircle(cell, playerTurn) {
+    if (cell) {
+        const circleRow = cell.parentElement.dataset.row;
+        const circleColumn = cell.dataset.column;
+        if (playerTurn === 1) {
             const circle = new YellowCircle(playerTurn);
-            board.boardArr[cell.parentElement.dataset.row][cell.dataset.column] = circle;
+
+            board.boardArr[circleRow][circleColumn] = circle;
             cell.style.backgroundColor = circle.color;
             console.log(board.boardArr);
         } else {
             const circle = new RedCircle(playerTurn);
-            board.boardArr[cell.parentElement.dataset.row][cell.dataset.column] = circle;
+            board.boardArr[circleRow][circleColumn] = circle;
             cell.style.backgroundColor = circle.color;
             console.log(board.boardArr);
         }
+        checkWinner(board.boardArr, playerTurn, circleColumn, circleRow);
         turn.changePlayer();
     }
+}
+
+function checkWinner(boardArr, circleColumn, playerTurn, circleRow) {
+    if (checkHorizontal(boardArr, playerTurn, circleColumn, circleRow))
+        //     checkVertical(boardArr,circleColumn,circleRow) ||
+        //     checkDiagonal(boardArr,circleColumn,circleRow)
+        // ) 
+{}
+}
+
+function checkHorizontal(boardArr, playerTurn, circleColumn, circleRow) {
+    if (boardArr[circleRow][circleColumn] instanceof Circle) {
+        const CircleType = playerTurn === 1 ? YellowCircle : RedCircle;
+        for (let i = 0; i < boardArr[circleRow].length - 3; i++) {
+            if (
+                boardArr[circleRow][i] instanceof CircleType &&
+                boardArr[circleRow][i] === boardArr[circleRow][i + 1] &&
+                boardArr[circleRow][i] === boardArr[circleRow][i + 2] &&
+                boardArr[circleRow][i] === boardArr[circleRow][i + 3]
+            ) {
+                console.log("Winner");
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function checkVertical(boardArr) {
+    for (let i = 0; i < boardArr[0].length; i++) {
+        for (let j = 0; j < boardArr.length - 3; j++) {
+            if (
+                boardArr[j][i] === boardArr[j + 1][i] &&
+                boardArr[j][i] === boardArr[j + 2][i] &&
+                boardArr[j][i] === boardArr[j + 3][i]
+            ) {
+                return true;
+            }
+        }
+    }
+}
+function checkDiagonal(boardArr) {
+    boardArr.forEach((row) => {
+        for (let i = 0; i < row.length - 3; i++) {
+            if (
+                row[i] === row[i + 1] &&
+                row[i] === row[i + 2] &&
+                row[i] === row[i + 3]
+            ) {
+                return true;
+            }
+        }
+    });
 }
